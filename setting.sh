adf673f415b6e03e37497dcc268ba59077f79c66
norif711 <50292204+norif711@users.noreply.github.com>
2023-10-13 21:47:12 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -6,7 +6,7 @@ ANSIBLE_PATH=~/Documents/10-study/git/m1-mac-ansible
 xcode-select --install
 
 ## install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## install ansible
 brew install ansible
