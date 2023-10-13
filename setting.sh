e023aeb4f029d4012181ff8edb392fc49a67b707
norif711 <50292204+norif711@users.noreply.github.com>
2023-10-13 22:04:32 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -7,6 +7,8 @@ xcode-select --install
 
 ## install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/noriakifujita/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 ## install ansible
 brew install ansible
